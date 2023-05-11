700bccfe4bf6d4e4329bd632b7a50618b7bfe661
Shailesh Thapa <shailesh.thapa@argano.com>
2023-05-11 10:24:01 -0400

    plans not visible when not logged in

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,6 +1,6 @@
 import MyNav from "./MyNav";
 import { StyleSheet } from 'react-native';
-import { Heading, Divider, useTheme } from '@aws-amplify/ui-react';
+import { Heading, Divider, useTheme, Alert } from '@aws-amplify/ui-react';
 
 
 
@@ -14,9 +14,14 @@ export default function Subscription(props){
                 <Heading level = {2} color = {tokens.colors.brand.primary[80]}><i>Subscription</i></Heading>
                 <Divider  border={`${tokens.borderWidths.large} solid ${tokens.colors.brand.primary[80]}`}/>
             </div>
-            <stripe-pricing-table 
+            {props.name === undefined ? <Alert variation='warning' 
+            style = {{fontSize:'x-large', maxWidth: '1200px', margin: '30px auto'}}>
+              Please login to access the subscription page
+            </Alert> :   <stripe-pricing-table 
             pricing-table-id="prctbl_1N6I7MIOs4Bwoex9um96LPkA" 
             publishable-key="pk_test_51HmobNIOs4Bwoex9HWRfjJt3gFCHUsBNOAMovIwXJfiOFVdFb2ahUfTljChjD2AjyJfk0KsHneueAA3vlP0NR5Hs00X7Q8CI37"></stripe-pricing-table>
+            }
+          
         </div>
     </div>)
 }
