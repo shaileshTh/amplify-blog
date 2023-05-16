a7547c1556c033e2a6ac110492ed837e07eca629
Shailesh Thapa <shailesh.thapa@argano.com>
2023-05-16 16:46:07 -0400

    my subscription page

--- src/components/MySubscription.js
+++ src/components/MySubscription.js
@@ -1,15 +1,15 @@
 import MyNav from "./MyNav";
 import { StyleSheet } from 'react-native';
-import { Heading, Divider, useTheme, Alert } from '@aws-amplify/ui-react';
-import { useEffect } from "react";
+import { Heading, Divider, Badge, Loader, useTheme, Alert } from '@aws-amplify/ui-react';
+import { useEffect, useState } from "react";
 const stripe = require('stripe')('sk_test_51HmobNIOs4Bwoex9erX63uEPKdwylY7a5f8zxvmskceNETVfJMaDzygagiugpV5xocQFVVjJEvHhslTdlHdExUs500dSOPhSrx');
-import { API } from 'aws-amplify';
 
 
 
 export default function MySubscription(props){
     const { tokens } = useTheme();
-    let product
+    const [subscriptionDetails, setSubscriptionDetails] = useState()
+
     useEffect(() => {
         if(props.customerId){
             const subscriptions = stripe.subscriptions.list({
@@ -17,23 +17,14 @@ export default function MySubscription(props){
             });
             
             subscriptions.then((r) => {
-                stripe.products.retrieve(r.data[0].plan.product).then(r => console.log(r))
+                stripe.products.retrieve(r.data[0].plan.product).then((r) => {
+                    setSubscriptionDetails(r)
+                })
             })
-        }
-       
-        if(props.username){
-            API.post("myAPI", "/get-subscription-status", {
-                body: {
-                  username: props.username
-                }
-              }).then((r) => {
-                console.log(r)
-              })
-            .catch(e => console.log(e))    
         }
         
     }, [props.customerId, props.username])
-    
+
     return(<div style = {{width:'100%', backgroundColor:'var(--amplify-colors-background-tertiary)'}}>
         <MyNav customerId = {props.customerId} page = 'subscription' name = {props.name}/>
         <div style = {styles.container}>
@@ -41,10 +32,32 @@ export default function MySubscription(props){
                 <Heading level = {2} color = {tokens.colors.brand.primary[80]}><i>My Subscription</i></Heading>
                 <Divider  border={`${tokens.borderWidths.large} solid ${tokens.colors.brand.primary[80]}`}/>
             </div>
+            <br/>
             {props.name === undefined ? <Alert variation='warning' 
             style = {{fontSize:'x-large', maxWidth: '1200px', margin: '30px auto'}}>
               Please login to access the subscription page
-            </Alert> :   <h1>My Subscription</h1> 
+            </Alert> :  <>
+            {subscriptionDetails ?
+                <Alert
+                    isDismissible={false}
+                    variation="info"
+                    heading={"Plan name: " + subscriptionDetails.name}
+                    >
+                {subscriptionDetails.active ? <Badge size = "small" variation="success">Plan Active</Badge>
+                :<Badge size = "small" variation="error">Plan Not Active</Badge>
+                }<br/><br/>
+                <b>Description:</b>
+                <br/>
+                {subscriptionDetails.description}
+                </Alert> : <Loader variation="linear"/>}
+        
+            <br/>
+               <button onClick = {() => {
+                    stripe.billingPortal.sessions.create({
+                        customer: props.customerId
+                    }).then(r => console.log(r.url))
+                }}>Manage Billing</button>
+            </>
             }
           
         </div>
